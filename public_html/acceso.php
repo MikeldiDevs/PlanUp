c5d343806adff8384e74d09f068c147025da039f
Uvemode <uvemode@mail.com>
2019-10-31 13:06:29 +0100

    Did I really leave the hardcoded credentials in a repository??

--- public_html/acceso.php
+++
@@ -1,8 +0,0 @@
-<?php
- define('SERVIDOR',"localhost");
-    define('USUARIO','phpmyadmin');
-    define('CLAVE','Proyecto@01');
-    define('BBDD',"planup");
-    ini_set("display_errors", "On");
-    error_reporting(E_ALL);
-?>
